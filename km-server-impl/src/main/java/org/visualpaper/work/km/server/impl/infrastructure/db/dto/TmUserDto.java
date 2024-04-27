ae0d80d3e9b06dac346a1a295efcaf16ee76b3f3
visualpaper <kazuya.umejima@gmail.com>
2024-04-28 05:44:11 +0900

    DB 実装 (#3)
    * DB 実装
    
    * 見直し
    
    * テストもいちおう

---
+++ km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/db/dto/TmUserDto.java
@@ -0,0 +1,36 @@
+package org.visualpaper.work.km.server.impl.infrastructure.db.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * TM_USER テーブル Entity.
+ */
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TM_USER")
+public class TmUserDto {
+
+  // 必要であれば BigInteger でやること。
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID")
+  private Integer id;
+
+  @Column(name = "NAME")
+  private String name;
+}
---
+++ km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/repository/UserRepositoryImpl.java
@@ -0,0 +1,55 @@
+package org.visualpaper.work.km.server.impl.infrastructure.repository;
+
+import jakarta.annotation.Nonnull;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.visualpaper.work.km.server.impl.domain.user.User;
+import org.visualpaper.work.km.server.impl.domain.user.UserId;
+import org.visualpaper.work.km.server.impl.domain.user.UserRepository;
+import org.visualpaper.work.km.server.impl.exceptions.KmException;
+import org.visualpaper.work.km.server.impl.infrastructure.db.dao.TmUserDao;
+import org.visualpaper.work.km.server.impl.infrastructure.db.dto.TmUserDto;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+  @Autowired
+  private TmUserDao dao;
+
+  @Nonnull
+  @Override
+  public List<User> findAll() throws KmException {
+    return dao.findAll().stream()
+        .map(this::toUser)
+        .collect(Collectors.toList());
+  }
+
+  @Nonnull
+  private User toUser(@Nonnull TmUserDto dto) {
+    return new User(UserId.from(dto.getId()), dto.getName());
+  }
+
+  @Override
+  public void insert(@Nonnull User user) throws KmException {
+    TmUserDto dto = new TmUserDto();
+    dto.setName(user.name());
+
+    dao.save(dto);
+  }
+
+  @Override
+  public void update(@Nonnull User user) throws KmException {
+    TmUserDto dto = new TmUserDto();
+    dto.setId(user.id().value());
+    dto.setName(user.name());
+
+    dao.save(dto);
+  }
+
+  @Override
+  public void delete(@Nonnull UserId userId) throws KmException {
+    dao.deleteById(userId.value());
+  }
+}

a9f075092ce85df0308b3e02ada98c7bc498c809
umejima <kazuya.umejima@gmail.com>
2024-04-28 05:42:54 +0900

    テストもいちおう

---
+++ km-server-impl/src/test/java/org/visualpaper/work/km/server/impl/resources/HealthCheckResourceTest.java
@@ -0,0 +1,32 @@
+// test.property に入れればできるのでメモだけ
+// IT はこんな感じで、
+
+//package org.visualpaper.work.km.server.impl.resources;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.mock.web.MockHttpServletResponse;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//@ActiveProfiles("test")
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//class HealthCheckResourceTest {
+//
+//  @Autowired
+//  private MockMvc mockMvc;
+//
+//  @Test
+//  void healthCheck() throws Exception {
+////    MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/healthCheck"))
+////        .andReturn()
+////        .getResponse();
+////
+////    Assertions.assertThat(response.getStatus()).isEqualTo(200);
+//  }
+//}

64b568686d45dae4cd49c1637382bfea0bee2024
visualpaper <kazuya.umejima@gmail.com>
2024-05-25 01:04:12 +0900

    ストアド実行サンプル(プロシージャ) (#10)

---
+++ km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/db/dao/StoredDao.java
@@ -0,0 +1,11 @@
+package org.visualpaper.work.km.server.impl.infrastructure.db.dao;
+
+import jakarta.annotation.Nonnull;
+import java.util.Map;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StoredDao {
+
+  void storedProcedureSample(@Nonnull Map<String, Object> params);
+}
---
+++ km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/resources/StoredSampleResource.java
@@ -0,0 +1,35 @@
+package org.visualpaper.work.km.server.impl.resources;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.visualpaper.work.km.server.impl.domain.user.UserId;
+import org.visualpaper.work.km.server.impl.infrastructure.repository.StoredSampleRepositoryImpl;
+
+@RestController
+public class StoredSampleResource {
+
+  @Autowired
+  private StoredSampleRepositoryImpl repository;
+
+  /**
+   * Stored Procedure Sample.
+   */
+  @Nonnull
+  @RequestMapping(
+      method = RequestMethod.GET,
+      value = "/v1/storedProcedureSample/{id}"
+  )
+  public ResponseEntity<Void> getUser(
+      @PathVariable("id") Integer id
+  ) throws Exception {
+    repository.storedProcedureSample(UserId.from(id));
+
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+}

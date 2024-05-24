1bf1d8b0930d9e6f9de0730db67062e735ef4af2
visualpaper <kazuya.umejima@gmail.com>
2024-05-24 22:59:10 +0900

    JPA 削除 (#8)

--- km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/db/dao/TmUserDao.java
+++ km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/db/dao/TmUserDao.java
@@ -1,12 +1,7 @@
 package org.visualpaper.work.km.server.impl.infrastructure.db.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.visualpaper.work.km.server.impl.infrastructure.db.dto.TmUserDto;
-
 /**
  * TM_USER テーブル DAO.
  */
-@Repository
-public interface TmUserDao extends JpaRepository<TmUserDto, Integer> {
+public interface TmUserDao {
 }
--- km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/repository/UserRepositoryImpl.java
+++ km-server-impl/src/main/java/org/visualpaper/work/km/server/impl/infrastructure/repository/UserRepositoryImpl.java
@@ -2,15 +2,7 @@ package org.visualpaper.work.km.server.impl.infrastructure.repository;
 
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.ParameterMode;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
-import jakarta.persistence.StoredProcedureQuery;
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.visualpaper.work.km.server.impl.domain.user.User;
@@ -23,91 +15,30 @@ import org.visualpaper.work.km.server.impl.infrastructure.db.dto.TmUserDto;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-  @PersistenceContext
-  private EntityManager em;
-
   @Autowired
   private TmUserDao dao;
 
-  /*
-   * StoredProcedure の例
   @Nullable
   @Override
   public User find(@Nonnull UserId id) throws KmException {
-    StoredProcedureQuery query = this.em.createStoredProcedureQuery("GET_USER_SAMPLE_PROCEDURE");
-    query.registerStoredProcedureParameter("target_id", Integer.class, ParameterMode.IN);
-    query.registerStoredProcedureParameter("id", Integer.class, ParameterMode.OUT);
-    query.registerStoredProcedureParameter("name", String.class, ParameterMode.OUT);
-    query.setParameter("target_id", id.value());
-    query.execute();
-
-    Integer resultId = (Integer) query.getOutputParameterValue("id");
-    String resultName = (String) query.getOutputParameterValue("name");
-    if (resultId == null) {
-      return null;
-    }
-    return new User(UserId.from(resultId), resultName);
-  }
-  */
-
-  /*
-   * StoredFunction の例
-   */
-  @Nullable
-  @Override
-  public User find(@Nonnull UserId id) throws KmException {
-    Query query = this.em.createNativeQuery(
-        "SELECT GET_USER_SAMPLE_FUNCTION(:target_id) from dual"
-    );
-    query.setParameter("target_id", id.value());
-    Object out = query.getSingleResult();
-
     return new User(UserId.from(1), "aaa");
   }
 
   @Nonnull
   @Override
   public List<User> findAll() throws KmException {
-    StoredProcedureQuery query = this.em.createStoredProcedureQuery("GET_USERS_SAMPLE_CURSOR_PROCEDURE");
-    query.registerStoredProcedureParameter("target_id", Integer.class, ParameterMode.IN);
-    query.registerStoredProcedureParameter("out_cur", Class.class, ParameterMode.REF_CURSOR);
-    query.setParameter("target_id", 41);
-    query.execute();
-
-    List<Object[]> results = query.getResultList();
-
-    return results.stream()
-        .map(this::toUser)
-        .collect(Collectors.toList());
-  }
-
-  @Nonnull
-  public User toUser(Object[] result) {
-    return new User(UserId.from(((BigDecimal) result[0]).intValue()), (String)result[1]);
-  }
-
-  /*
-   * とりあえず退避
-  @Nonnull
-  @Override
-  public List<User> findAll() throws KmException {
-    return dao.findAll().stream()
-        .map(this::toUser)
-        .collect(Collectors.toList());
+    return List.of();
   }
 
   @Nonnull
   private User toUser(@Nonnull TmUserDto dto) {
     return new User(UserId.from(dto.getId()), dto.getName());
   }
-  */
 
   @Override
   public void insert(@Nonnull User user) throws KmException {
     TmUserDto dto = new TmUserDto();
     dto.setName(user.name());
-
-    dao.save(dto);
   }
 
   @Override
@@ -115,12 +46,9 @@ public class UserRepositoryImpl implements UserRepository {
     TmUserDto dto = new TmUserDto();
     dto.setId(user.id().value());
     dto.setName(user.name());
-
-    dao.save(dto);
   }
 
   @Override
   public void delete(@Nonnull UserId userId) throws KmException {
-    dao.deleteById(userId.value());
   }
 }
